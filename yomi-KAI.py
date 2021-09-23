abb68c80d17c063b1b5317862dd4fd4cb85aca9d
garden_tree <keigo3142011@yahoo.co.jp>
2021-09-23 14:37:10 +0900

    ディレクトリ自動作成、ネタバレ置換、URL置換を修正、自動切断の条件をゆるく、readme

--- yomi-KAI.py
+++ yomi-KAI.py
@@ -13,34 +13,39 @@ import pprint
 import glob
 from logging import StreamHandler, FileHandler, Formatter, basicConfig, getLogger, INFO, DEBUG, NOTSET
 
-# ストリームハンドラの設定
+# ディレクトリ作成
+if not os.path.isdir("dict"):
+    os.mkdir("dict")
+if not os.path.isdir("log"):
+    os.mkdir("log")
+if not os.path.isdir("temp"):
+    os.mkdir("temp")
+
+# ログの設定
+format = "[%(asctime)s] %(name)s:%(lineno)s %(funcName)s [%(levelname)s]: %(message)s"
+
 sh = StreamHandler()
 sh.setLevel(INFO)
-sh.setFormatter(Formatter("[%(asctime)s] %(name)s:%(lineno)s %(funcName)s [%(levelname)s]: %(message)s"))
+sh.setFormatter(Formatter(format))
 
-# 保存先の有無チェック
-if not os.path.isdir('./log'):
-    os.makedirs('./log', exist_ok=True)
-
-fh = FileHandler(f"./log/{datetime.now():%Y-%m-%d_%H%M%S}.log", encoding="utf-8")
+fh = FileHandler(f"./log/{datetime.now():%Y-%m-%d_%H%M%S}.log", encoding="UTF-8")
 fh.setLevel(DEBUG)
-fh.setFormatter(Formatter("[%(asctime)s] %(name)s:%(lineno)s %(funcName)s [%(levelname)s]: %(message)s"))
+fh.setFormatter(Formatter(format))
 
-# ルートロガーの設定
 basicConfig(level=NOTSET, handlers=[sh, fh])
 logger = getLogger(__name__)
 
-#起動時に./temp内の*.wavをすべて削除
+# 起動時に./temp内の*.wavをすべて削除
 for filename in glob.glob("./temp/*.wav"):
     os.remove(filename)
 
-#ログを10個残す
+# ログを10個残す
 file_list = glob.glob("./log/*.log")
 if len(file_list) > 10:
     for i in range(len(file_list)-10):
         os.remove(file_list[i])
 
-#setting.jsonの確認
+# setting.jsonの確認
 try:
     with open("settings.json", "r", encoding="UTF-8") as f:
         settings = json.load(f)
@@ -51,7 +56,7 @@ except:
     logger.exception("setting.jsonが見つかりません。")
     sys.exit()
 
-#VOICECETEXT_API_KEYの確認
+# VOICECETEXT_API_KEYの確認
 try:
     vt = VoiceText(VOICETEXT_API_KEY)
 except:
@@ -60,7 +65,7 @@ except:
 
 check_text_channel = None
 
-#キュー
+# キュー
 queue_dict = defaultdict(deque)
 
 def enqueue(voice_client, guild, source):
@@ -79,15 +84,15 @@ intents = discord.Intents.default()
 intents.members = True 
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
-#接続時の処理
+# 接続時の処理
 @bot.event
 async def on_ready():
     logger.info(f"Launch complete! Logged in as [{bot.user.name}]. PREFIX is [{PREFIX}].")
 
-#もとからあるhelpコマンドを無効化
+# もとからあるhelpコマンドを無効化
 bot.remove_command('help')
 
-#ヘルプコマンド
+# ヘルプコマンド
 @bot.command()
 async def help(ctx):
     embed = discord.Embed(title="yomi-KAI", description="テキスト読み上げbotです。", inline="false")
@@ -98,7 +103,7 @@ async def help(ctx):
     await ctx.send(embed=embed)
     logger.info("helpを表示")
 
-#ボイスチャンネルに接続
+# ボイスチャンネルに接続
 @bot.command()
 async def c(ctx):
     if ctx.author.voice is None:
@@ -120,7 +125,7 @@ async def c(ctx):
     await ctx.channel.send(f"{ctx.author.voice.channel.name}に接続しました")
     logger.info(f"{ctx.author.voice.channel.name}に接続しました")
 
-#ボイスチャンネルから切断
+# ボイスチャンネルから切断
 @bot.command()
 async def dc(ctx):
     if ctx.guild.voice_client is None:
@@ -132,7 +137,7 @@ async def dc(ctx):
     await ctx.channel.send("切断しました")
     logger.info(f"切断しました")
 
-#辞書
+# 辞書
 @bot.command()
 async def dict(ctx, *args):
     if os.path.isfile(f"./dict/{ctx.guild.id}.json") == True:
@@ -177,64 +182,67 @@ async def dict(ctx, *args):
         await ctx.channel.send(f"コマンドが間違っています。`{PREFIX}dict help`を参照してください")
         logger.info(f"コマンドが間違っています。`{PREFIX}dict help`を参照してください")
 
-#メッセージが送られた時
+# メッセージが送られた時
 @bot.event
 async def on_message(message):
-    #コマンドをコマンドとしてトリガーし、読み上げから除外
+    # コマンドをコマンドとしてトリガーし、読み上げから除外
     if message.content.startswith(PREFIX):
         await bot.process_commands(message)
         return
 
-    #botの発言は無視
+    # botの発言は無視
     if message.author.bot:
         return
 
-    #読み上げ
+    # 読み上げ
     if message.channel == check_text_channel and message.guild.voice_client is not None:
-        #URL置換
-        read_msg = re.sub(r"https?://.*", "URL", message.content)
+        read_msg = message.content
 
-        #メンション置換
+        # 辞書置換
+        if os.path.isfile(f"./dict/{message.guild.id}.json") == True:
+            with open(f"./dict/{message.guild.id}.json", "r", encoding="UTF-8")as f:
+                word = json.load(f)
+            read_list = [] # あとでまとめて変換するときの読み仮名リスト
+            for i, one_dic in enumerate(word.items()): # one_dicは単語と読みのタプル。添字はそれぞれ0と1。
+                read_msg = read_msg.replace(one_dic[0], '{'+str(i)+'}')
+                read_list.append(one_dic[1]) # 変換が発生した順に読みがなリストに追加
+            read_msg = read_msg.format(*read_list) # 読み仮名リストを引数にとる
+
+        # URL置換
+        read_msg = re.sub(r"https?://.*?\s|https?://.*?$", "URL", read_msg)
+
+        # ネタバレ置換
+        read_msg = re.sub(r"\|\|.*?\|\|", "ネタバレ", read_msg)
+
+        # メンション置換
         if "<@" and ">" in message.content:
-            P = "<@!?([0-9]+)>" #パターン
-            Temp = re.findall(P, message.content)
+            Temp = re.findall("<@!?([0-9]+)>", message.content)
             for i in range(len(Temp)):
                 Temp[i] = int(Temp[i])
                 user = message.guild.get_member(Temp[i])
                 read_msg = re.sub(f"<@!?{Temp[i]}>", "アット" + user.display_name, read_msg)
 
-        #辞書置換
-        if os.path.isfile(f"./dict/{message.guild.id}.json") == True:
-            with open(f"./dict/{message.guild.id}.json", "r", encoding="UTF-8")as f:
-                word = json.load(f)
-
-            read_list = [] # あとでまとめて変換するときの読み仮名リスト
-            for i, one_dic in enumerate(word.items()): # one_dicは単語と読みのタプル。添字はそれぞれ0と1。
-                read_msg = read_msg.replace(one_dic[0], '{'+str(i)+'}')
-                read_list.append(one_dic[1]) # 変換が発生した順に読みがなリストに追加
-            read_msg = read_msg.format(*read_list) #読み仮名リストを引数にとる
-
-        #音声ファイル作成
+        # 音声ファイル作成
         gen_time = datetime.now().strftime("%Y-%m-%d_%H%M%S")
-        with open(f"./temp/{gen_time}.wav","wb", encoding="UTF-8") as f:
+        with open(f"./temp/{gen_time}.wav","wb") as f:
             f.write(vt.speed(120).to_wave(read_msg))
 
-        #音声読み上げ
+        # 音声読み上げ
         enqueue(message.guild.voice_client, message.guild, discord.FFmpegPCMAudio(f"./temp/{gen_time}.wav"))
         logger.info(f"ReadSentence:{read_msg}")
 
-        #音声ファイル削除
+        # 音声ファイル削除
         with wave.open(f"./temp/{gen_time}.wav", "rb")as f:
-            wave_length=(f.getnframes() / f.getframerate()) #再生時間 
+            wave_length=(f.getnframes() / f.getframerate()) # 再生時間 
         logger.info(f"PlayTime:{wave_length}")
         await asyncio.sleep(wave_length + 10)
 
         os.remove(f"./temp/{gen_time}.wav")
 
-#誰も居なくなると自動切断
+# 誰も居なくなると自動切断
 @bot.event
 async def on_voice_state_update(member, before, after):
-    if (member.guild.voice_client is not None and after.channel is None and member.id != bot.user.id and member.guild.voice_client.channel is before.channel and len(member.guild.voice_client.channel.members) == 1):
+    if (member.guild.voice_client is not None and member.id != bot.user.id and member.guild.voice_client.channel is before.channel and len(member.guild.voice_client.channel.members) == 1):
         await member.guild.voice_client.disconnect()
         await check_text_channel.send("自動切断しました")
         logger.info(f"自動切断しました")
