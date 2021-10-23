7d722f5aa66a6a22e4b9aba5bcd87da7469acec0
garden_tree <keigo3142011@yahoo.co.jp>
2021-10-23 21:12:16 +0900

    fix build, env.build

--- build.bat
+++ build.bat
@@ -4,3 +4,4 @@ copy windows-run.bat yomi-KAI-v
 rem copy linux-run.sh yomi-KAI-v
 copy yomi-KAI.py yomi-KAI-v
 copy settings.json.example yomi-KAI-v
+copy env_build.ps1 yomi-KAI-v
--- env_build.ps1
+++ env_build.ps1
@@ -1,8 +1,8 @@
 Invoke-WebRequest "https://www.python.org/ftp/python/3.9.7/python-3.9.7-embed-amd64.zip" -OutFile "python-3.9.7-embed-amd64.zip"
 Expand-Archive python-3.9.7-embed-amd64.zip
 Set-Location .\python-3.9.7-embed-amd64
-$data = Get-Content python39._pth | foreach { $_ -replace '#import','import' }
-$data | Out-String | % { [Text.Encoding]::UTF8.GetBytes($_) } | Set-Content -Path "python39._pth" -Encoding Byte
+$data = Get-Content python39._pth | ForEach-Object { $_ -replace '#import','import' }
+$data | Out-String | ForEach-Object { [Text.Encoding]::UTF8.GetBytes($_) } | Set-Content -Path "python39._pth" -Encoding Byte
 Invoke-WebRequest "https://bootstrap.pypa.io/get-pip.py" -OutFile "get-pip.py"
 .\python.exe .\get-pip.py
 .\python.exe -m pip install discord.py[voice]
